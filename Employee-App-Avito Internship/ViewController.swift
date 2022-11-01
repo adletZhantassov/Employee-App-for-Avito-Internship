15fa30adceed4c66ab971942b81f1fa20413d50f
Adlet <adlet_1996@mail.ru>
2022-11-02 01:01:28 +0600

    Initial Commit

---
+++ Employee-App-Avito Internship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employee-App-Avito Internship
+//
+//  Created by Adlet Zhantassov on 02.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
